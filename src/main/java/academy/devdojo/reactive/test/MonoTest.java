f08f45e3fa6450be2d29b5c86a4083d398af0d07
Carlos Eduardo <carlos.germano@dce.ufpb.br>
2021-01-20 12:35:52 -0300

    first commit

---
+++ src/main/java/academy/devdojo/reactive/test/MonoTest.java
@@ -0,0 +1,40 @@
+package academy.devdojo.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@Slf4j
+/**
+ * Reactive Streams
+ * 1. Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ * Publisher <- (subscribe) Subscriber
+ * Subscription is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * Publisher -> (onNext) Subscriber
+ * util:
+ * 1. Publisher sends all the objects requested.
+ * 2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled.
+ * 3. There is an error. (onError) -> subscriber and subscription will be canceled.
+ */
+public class MonoTest {
+
+    @Test
+    public void monoSubscriber() {
+        String name = "Carlos Eduardo";
+        Mono<String> mono = Mono.just(name)
+                .log();
+
+        mono.subscribe();
+
+        log.info("---------------------------------------");
+
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
+    }
+}
